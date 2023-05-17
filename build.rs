611d7029f0cedb9896be1920d5dced872fd70cee
c.lamboo <casperlamboo@gmail.com>
2023-05-17 16:58:15 +0200

    Download protobuf defintions from seperate repo

--- build.rs
+++ build.rs
@@ -1,6 +1,23 @@
 use std::io::Result;
+use std::process::Command;
 
 fn main() -> Result<()> {
-    tonic_build::compile_protos("src/plugin.proto")?;
+    // Download the grpc definitions
+    Command::new("git")
+        .args([
+            "clone",
+            "git@github.com:Ultimaker/curaengine_grpc_defintions.git",
+        ])
+        .output()?;
+
+    // Compile the proto files
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .compile(
+            &["plugin.proto", "simplify.proto", "postprocess.proto"],
+            &["./curaengine_grpc_defintions"],
+        )?;
+
     Ok(())
 }
